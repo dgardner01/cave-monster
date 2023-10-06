dc7521d6d11db3713de213b65ad3a69556c8b039
dgardner01 <98490766+dgardner01@users.noreply.github.com>
2023-10-06 15:20:59 -0400

    part 4

--- src/procgen.ts
+++ src/procgen.ts
@@ -1,5 +1,5 @@
-import { FLOOR_TILE, WALL_TILE, Tile } from './tile-types';
 import { GameMap } from './game-map';
+import { FLOOR_TILE, WALL_TILE, Tile } from './tile-types';
 import { Display } from 'rot-js';
 import { Entity } from './entity';
 
@@ -28,6 +28,12 @@ class RectangularRoom {
         }
     }
 
+    get center(): [number, number] {
+        const centerX = this.x + Math.floor(this.width / 2);
+        const centerY = this.y + Math.floor(this.height / 2);
+        return [centerX, centerY];
+    }
+
     intersects(other: RectangularRoom): boolean {
         return (
             this.x <= other.x + other.width &&
@@ -35,12 +41,6 @@ class RectangularRoom {
             this.y <= other.y + other.height &&
             this.y + this.width >= other.y
         );
-    }
-
-    public get center(): [number, number] {
-        const centerX = this.x + Math.floor(this.width / 2);
-        const centerY = this.y + Math.floor(this.height / 2);
-        return [centerX, centerY];
     }
 }
 
@@ -54,6 +54,7 @@ export function generateDungeon(
     display: Display,
 ): GameMap {
     const dungeon = new GameMap(mapWidth, mapHeight, display);
+
     const rooms: RectangularRoom[] = [];
 
     for (let count = 0; count < maxRooms; count++) {
@@ -70,6 +71,7 @@ export function generateDungeon(
         }
 
         dungeon.addRoom(x, y, newRoom.tiles);
+
         rooms.push(newRoom);
     }
 
@@ -77,10 +79,10 @@ export function generateDungeon(
     player.x = startPoint[0];
     player.y = startPoint[1];
 
-
     for (let index = 0; index < rooms.length - 1; index++) {
         const first = rooms[index];
         const second = rooms[index + 1];
+
         for (let tile of connectRooms(first, second)) {
             dungeon.tiles[tile[1]][tile[0]] = { ...FLOOR_TILE };
         }
@@ -89,37 +91,38 @@ export function generateDungeon(
     return dungeon;
 }
 
+function generateRandomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 function* connectRooms(
     a: RectangularRoom,
     b: RectangularRoom,
 ): Generator<[number, number], void, void> {
-   //set the start point of our tunnel at the center of the first room
+    // set the start point of our tunnel at the center of the first room
     let current = a.center;
-    //set the end point at the center of the second room
+    // set the end point at the center of the second room
     const end = b.center;
 
-    //flip a coin to see if we go horizontally first or vertically
+    // flip a coin to see if we go horizontally first or vertically
     let horizontal = Math.random() < 0.5;
-    //set our axisindex to 0 (x axis) if horizontal or 1 (y axis) if vertical
+    // set our axisIndex to 0 (x axis) if horizontal or 1 (y axis) if vertical
     let axisIndex = horizontal ? 0 : 1;
 
-    //we'll loop until our current is the same as the end point
+    console.log(current, end);
+    // we'll loop until our current is the same as the end point
     while (current[0] !== end[0] || current[1] !== end[1]) {
-        //are we tunneling in positive or negative direction?
-        //if direction is - we have hit the destinaton in one direction
+        //are we tunneling in the positive or negative direction?
+
+        // if direction is 0 we have hit the destination in one direction
         const direction = Math.sign(end[axisIndex] - current[axisIndex]);
         if (direction !== 0) {
             current[axisIndex] += direction;
             yield current;
         } else {
-            //we've finished in this direction so switch to other
+            // we've finished in this direction so switch to the other
             axisIndex = axisIndex === 0 ? 1 : 0;
             yield current;
         }
     }
 }
-
-function generateRandomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
--- src/tile-types.ts
+++ src/tile-types.ts
@@ -7,17 +7,26 @@ export interface Graphic {
 export interface Tile {
     walkable: boolean;
     transparent: boolean;
+    visible: boolean;
+    seen: boolean;
     dark: Graphic;
+    light: Graphic;
 }
 
 export const FLOOR_TILE: Tile = {
     walkable: true,
     transparent: true,
-    dark: { char: '.', fg: '#1b0202', bg: '#3c0505' },
+    visible: false,
+    seen: false,
+    dark: { char: '*', fg: '#000000', bg: '#000000' },
+    light: { char: '.', fg: '#aa0000', bg: '#ff5555' },
 };
 
 export const WALL_TILE: Tile = {
-    dark: { char: '~', fg: '#1b0202', bg: '#000' },
     walkable: false,
     transparent: false,
+    visible: false,
+    seen: false,
+    dark: { char: '*', fg: '#aa0000', bg: '#000000' },
+    light: { char: '#', fg: '#ff5555', bg: '#aa0000' },
 };
